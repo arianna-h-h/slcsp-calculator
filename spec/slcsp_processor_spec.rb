3681b42491f3b9aa6f97cbcfd6dc5ba8420447b7
arianna haradon <ariannaharadon@gmail.com>
2024-06-25 17:08:57 -0400

    inital commit

---
+++ spec/slcsp_processor_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../slcsp_processor'
+
+describe 'MyScript' do
+  it 'performs some action correctly' do
+    # Test your script's behavior here
+    puts "test runnign"
+    # expect(MyScript.some_method(input)).to eq(expected_output)
+  end
+
+  # Add more tests as needed
+end
